73088344ea005939b79005f8943f341105be40d9
Jeremy Ellis <keyfreemusic@gmail.com>
2020-08-31 19:43:22 -0700

    Update a02-tfjs-read.json.sh

--- myExample/a02-tfjs-read.json.sh
+++ myExample/a02-tfjs-read.json.sh
@@ -3,7 +3,7 @@
 
 echo "Problem: When you change the file name of a TFJS model with it's shard files, you also must change the PATHS in the .json file"
 echo "Easier to not change the models file names"
-echo "Enter the name of your TensorflowJS .json saved model with the .json exctension, example: model.json"
+echo "Enter the name of your TensorflowJS .json saved model with the .json extension, example: model.json"
 read myFile
 echo $myFile
 
