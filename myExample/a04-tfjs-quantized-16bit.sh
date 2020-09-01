f53d292bb8d6a05f56eef6e32afe633baaa201d6
Jeremy Ellis <keyfreemusic@gmail.com>
2020-08-31 20:36:14 -0700

    Update a04-tfjs-quantized-16bit.sh

--- myExample/a04-tfjs-quantized-16bit.sh
+++ myExample/a04-tfjs-quantized-16bit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tensorflowjs_converter --input_format=tfjs_layers_model  --quantization_bytes 2 --output_format=keras_saved_model ./model.json ./
+tensorflowjs_converter --input_format=tfjs_layers_model  --quantize_uint16 --output_format=keras_saved_model ./model.json ./
 tflite_convert --keras_model_file ./ --output_file ./model.tflite
 xxd -i model.tflite model.h
