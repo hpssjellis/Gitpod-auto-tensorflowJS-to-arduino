1cebc579d471fb15e531beef35bdc3e1f9f10441
Jeremy Ellis <keyfreemusic@gmail.com>
2020-09-04 19:08:10 -0700

    Delete a05-quantized-float16.sh

--- myExample/a05-quantized-float16.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tensorflowjs_converter --input_format=tfjs_layers_model  --quantize_float16=* --output_format=keras_saved_model ./model.json ./
-tflite_convert --keras_model_file ./ --output_file ./model.tflite
-xxd -i model.tflite model.h
