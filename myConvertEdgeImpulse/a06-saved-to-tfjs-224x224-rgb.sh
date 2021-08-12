04c03005a48e0e185187233eb590a70873d27dec
Jeremy Ellis <keyfreemusic@gmail.com>
2021-08-11 21:30:34 -0700

    Update a06-saved-to-tfjs-224x224-rgb.sh

--- myConvertEdgeImpulse/a06-saved-to-tfjs-224x224-rgb.sh
+++ myConvertEdgeImpulse/a06-saved-to-tfjs-224x224-rgb.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 
 unzip ei-ei-0unknown-1heartrock-v0.1.1-transfer-learning-tensorflow-savedmodel-model.zip
-tensorflowjs_converter --input_format=tf_saved_model  ./saved_model/ ./for_tfjs/
+tensorflowjs_converter --input_format=tf_saved_model  ./saved_model/ ./for_tfjs_224x224_rgb/
