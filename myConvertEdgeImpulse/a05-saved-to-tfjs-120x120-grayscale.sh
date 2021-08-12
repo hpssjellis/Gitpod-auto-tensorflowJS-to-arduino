167cc26146c3d03aede8c4553e4a43f0ea06c9c7
Jeremy Ellis <keyfreemusic@gmail.com>
2021-08-11 21:27:35 -0700

    Update and rename a05-saved-to-tfjs.sh to a05-saved-to-tfjs-120x120-grayscale.sh

--- myConvertEdgeImpulse/a05-saved-to-tfjs-120x120-grayscale.sh
+++ myConvertEdgeImpulse/a05-saved-to-tfjs-120x120-grayscale.sh
@@ -1,4 +1,6 @@
 
+#!/bin/bash
+
 unzip ei-ei-0unknown-1heartrock-v0.0.1-transfer-learning-tensorflow-savedmodel-model.zip
 tensorflowjs_converter --input_format=tf_saved_model  ./saved_model/ ./for_tfjs/
 
